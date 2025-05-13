83812dc37be2fdd8595b07ee529753707c2ea21e
Eric VAllejo <ejr15090@gmail.com>
2025-05-13 10:52:38 -0600

    Week 2 Assignment completed

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return(
+        <div>
+            <p>Eric Vallejo</p>
+            <Link href="https://github.com/ervgio/cprg306-assignments">My Github</Link>
+        </div>
+    );
+}

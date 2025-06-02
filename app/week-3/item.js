17e9d253502e9825573cafdc832d149facd9846c
Eric vallejo <Eric.VallejoJr@edu.sait.ca>
2025-06-02 15:47:14 -0600

    updated again

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,7 +1,7 @@
 export default function Item(props) {
   return (
-    <li className="p-2 m-4 bg-slate-900 max-w-sm" >
-      <p className="text-xl font-bold">{props.name}</p>
+    <li className="p-3 m-6 bg-red-500 max-w-sm ">
+      <p className="text-xl font-bold text">{props.name}</p>
       <p className="text-sm">Buy {props.quantity} in {props.category}</p>
     </li>
   );
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -3,7 +3,7 @@ import ItemList from "./item-list";
 export default function Page() {
   return (
     <main>
-      <h1 className="text-3xl font-bold m-2">Shopping List</h1>
+      <h1 className="text-3xl font-bold m-5.5 ">Shopping List</h1>
         <ItemList/>
     </main>
   );
